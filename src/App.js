8cc25399628a46878d48b8c27719619998d80ffa
Diego Diniz <diegodinizz@gmail.com>
2020-07-01 14:19:39 +0100

    array of monsters created

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: '1'
+        },
+        {
+          name: 'Dracula',
+          id: '2'
+        },
+        {
+          name: 'Zombie',
+          id: '3'
+        }
+      ]
+    }
+  }
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        {this.state.monsters.map(monster => (
+          <p key={monster.id}>{monster.name}</p>
+        ))}
       </div>
-    );
+    )
   }
 }
 
