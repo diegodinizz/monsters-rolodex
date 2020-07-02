61e413998e46ef8ac97d3673e2530e8fe267c12b
Diego Diniz <diegodinizz@gmail.com>
2020-07-02 15:39:37 +0100

    Updates

---
+++ precache-manifest.8c01eed0451f082b60cb8d117431bec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9605335e7d64df32381b74e2a20f7dbc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "25eeff9c70eeb97fdc22",
+    "url": "/monsters-rolodex/static/css/main.1671c037.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25eeff9c70eeb97fdc22",
+    "url": "/monsters-rolodex/static/js/main.76738356.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
